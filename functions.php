93ba7d6502f0b52818ba774e15677a1924804c71
Craig D. Miller <cmiller@hampshire.edu>
2024-04-16 12:00:49 -0400

    fix syntax error in : Give a pink background in wordpress header if logged into a .dev site, otherwise the normal dark background (fixed the hardcoded URL)

--- functions.php
+++ functions.php
@@ -52,7 +52,7 @@ add_action('save_post', 'dhamma_meta_save');
 
 function is_dev() {
   $url = parse_url( get_site_url(), PHP_URL_HOST );
-  returns str_contains( $url, ".dev." ); // is URL nyus.dev.webhost2.dhamma.org or dhara.dev.webhost2.dhamma.org ?
+  return str_contains( $url, ".dev." ); // is URL nyus.dev.webhost2.dhamma.org or dhara.dev.webhost2.dhamma.org ?
 }
 
 function is_restricted() {
